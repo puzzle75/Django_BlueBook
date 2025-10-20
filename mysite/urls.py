6f41d836a69be2eb1f41e98e8c8f82c69cc1c29a
puzzle75 <puzzle75@naver.com>
2025-10-20 16:31:53 +0900

    CH3까지 완료

---
+++ mysite/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('bookmark/', include('bookmark.urls')),
+    path('blog/', include('blog.urls')),
+]
